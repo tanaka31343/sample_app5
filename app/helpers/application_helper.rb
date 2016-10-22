43493f460167ab725b5e45b4a6452d418f664a8c
takuya_kimura <sunaosaimyo@gmail.com>
2016-10-22 06:54:54 +0100

    finish 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #ページごとの完全なタイトルを返す
+  def full_title(pages_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if pages_title.empty?
+      base_title
+    else
+      pages_title + " | " + base_title
+    end
+  end
 end
