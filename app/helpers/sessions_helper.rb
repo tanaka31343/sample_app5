2819417597470d4c8ec1fed194c37548d5680bfe
takuya_kimura <sunaosaimyo@gmail.com>
2016-10-23 02:55:04 +0100

    finish 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -36,5 +40,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
